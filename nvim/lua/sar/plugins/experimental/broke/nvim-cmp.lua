f50c5694a4a1226f79b034571be4fbb3c54de0b2
sgtgaaaf <147757120+sgtgaaaf@users.noreply.github.com>
2024-06-25 15:05:20 +0100

    error correction

--- nvim/lua/sar/plugins/experimental/broke/nvim-cmp.lua
+++ nvim/lua/sar/plugins/experimental/broke/nvim-cmp.lua
@@ -1,11 +1,13 @@
 return {
-   "hrsh7th/cmp-nvim-lsp",
-   "hrsh7th/cmp-buffer",
-   "hrsh7th/cmp-path",
-   "hrsh7th/cmp-cmdline",
    "hrsh7th/nvim-cmp",
-   "L3MON4D3/LuaSnip",
-   "saadparwaiz1/cmp_luasnip",
+   dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+   },
    config = function()
       local cmp = require"cmp"
       cmp.setup({
@@ -17,8 +19,8 @@ return {
             end,
          },
          window = {
-            -- completion = cmp.config.window.bordered(),
-            -- documentation = cmp.config.window.bordered(),
+            completion = cmp.config.window.bordered(),
+            documentation = cmp.config.window.bordered(),
          },
          mapping = cmp.mapping.preset.insert({
             ["<C-b>"] = cmp.mapping.scroll_docs(-4),
@@ -70,8 +72,6 @@ return {
       require("lspconfig")["lua_ls"].setup {
          capabilities = capabilities
       }
-
    end
 }
-
 
--- nvim/lua/sar/plugins/treesitter.lua
+++ nvim/lua/sar/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
    "nvim-treesitter/nvim-treesitter",
    config = function()
       require("nvim-treesitter.configs").setup({
-         ensure_installed = { "lua", "java", "typescript", "c", "vim",},
+         ensure_installed = { "java", "typescript", "c", "vim",},
          auto_install = true,
          highlight = {
             enable = true
