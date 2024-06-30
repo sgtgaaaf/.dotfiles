4be92ea6d0cb85353c6c0927580faee3e932db1f
sgtgaaaf <sarthakghimire@gmail.com>
2024-06-30 21:29:14 +0100

    illuminate

---
+++ nvim/lua/sar/plugins/experimental/disable/smooth-scroll.lua
@@ -0,0 +1,14 @@
+return {
+ {
+    "karb94/neoscroll.nvim",
+    config = function()
+      require("neoscroll").setup({
+        stop_eof = true,
+        easing_function = "sine",
+        hide_cursor = true,
+        cursor_scrolls_alone = true,
+      })
+    end,
+  },
+
+}
