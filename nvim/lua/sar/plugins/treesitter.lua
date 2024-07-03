78f4e7f94b7465d5042b625b47da095b333c6b16
sgtgaaaf <147757120+sgtgaaaf@users.noreply.github.com>
2024-07-03 16:46:39 +0100

    trsitter debug

--- nvim/lua/sar/plugins/treesitter.lua
+++ nvim/lua/sar/plugins/treesitter.lua
@@ -1,12 +1,30 @@
 return {
    "nvim-treesitter/nvim-treesitter",
    config = function()
-      require("nvim-treesitter.configs").setup({
-         ensure_installed = { "java",  "javascript", "typescript", "c", "vim", "lua" },
+      local status ,ts = pcall(require,"nvim-treesitter.configs") 
+      if not status then
+         return
+      end
+      ts.setup({
+         ensure_installed = { "java",  "javascript", "typescript", "c", "vim", "lua", "markdown", "toml", "json", "xml", "html" },
+         
          auto_install = true,
          highlight = {
             enable = true
-         }
+         },
+         context_commentstring = {
+            enable = true,
+            enable_autocmd = false,
+         },
+         rainbow = {
+            enable = true,
+            disable = { "html" },
+            extended_mode = false,
+            max_file_lines = nil,
+         },
+         autotag = { enable = true },
+         incremental_selection = { enable = true },
+         indent = { enable = true },
       })
    end
 }
