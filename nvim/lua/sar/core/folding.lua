2750638f45f8a8b184937e231c2291f00d214403
sgtgaaaf <sarthakghimire@gmail.com>
2024-07-06 10:24:22 +0100

    specs for folding added syntax still dont work

---
+++ nvim/lua/sar/core/folding.lua
@@ -0,0 +1,11 @@
+-- fold
+-- vim.cmd(":set foldmethod=syntax")
+vim.o.foldmethod = "syntax"
+-- visuals
+vim.cmd(":highlight Folded guibg=#3a3a3a guifg=none")
+vim.cmd(":highlight FoldColumn guibg=#292a2f guifg=#73797E")
+-- foldtext
+-- vim.cmd(":set foldtext = getline(v:foldstart)")
+-- vim.opt.foldclose=">"
+-- vim.cmd("set fillchars=stl:\ ,stlnc:\ ,vert:│,fold:·,diff:-")
+vim.opt.fillchars="fold: ,foldclose:›,foldsep: ,foldopen:⌄,fold: "
