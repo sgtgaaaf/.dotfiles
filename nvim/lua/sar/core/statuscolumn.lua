223a1bafebdd6553019f8b7268a745d2d09af9d1
sgtgaaaf <sarthakghimire@gmail.com>
2024-07-08 20:41:51 +0100

    alacritty config added

--- nvim/lua/sar/core/statuscolumn.lua
+++ nvim/lua/sar/core/statuscolumn.lua
@@ -1,7 +1,7 @@
 -- statuscolumn
 -- vim.opt.statuscolumn="%@SignCb@%s%=%T%@NumCb@%{v:relnum?v:relnum:v:lnum}  %C▕%T"
 vim.opt.statuscolumn="%@SignCb@%s%=%T%@NumCb@%{v:relnum?v:relnum:v:lnum}  %C│%T"
-
+-- %s%=%{v:lnum} %#FoldColumn#%@v:lua.StatusColumn.handler.fold@%{v:lua.StatusColumn.display.fold()}%#StatusColumnBorder#▐%#StatusColumnBuffer#
 -- vim.cmd("set statuscolumn=%@SignCb@%s%=%T%@NumCb@%{v:lnum?}%r│%T")
 -- vim.opt.statuscolumn="%s%T %{v:relnum?v:relnum:v:lnum}  %C│%T"
 -- vim.g.statuscolumn="%=%{v:relnum?v:relnum:v:lnum} "
