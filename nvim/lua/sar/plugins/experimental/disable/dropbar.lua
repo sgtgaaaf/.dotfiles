3dcb179d2fe24b97792a1961e3a2d96eb6637ec1
sgtgaaaf <sarthakghimire@gmail.com>
2024-07-21 13:58:31 +0100

    misc.

--- nvim/lua/sar/plugins/experimental/disable/dropbar.lua
+++ nvim/lua/sar/plugins/experimental/disable/dropbar.lua
@@ -1,4 +1,4 @@
-return {
+return { -- https://www.github.com/Bekaboo/dropbar.nvim
    'Bekaboo/dropbar.nvim',
    -- optional, but required for fuzzy finder support
    dependencies = {
--- nvim/lua/sar/plugins/formatter.lua
+++ nvim/lua/sar/plugins/formatter.lua
@@ -1,21 +1,22 @@
 return {
-   "stevearc/conform.nvim",
-   config = function()
-      require("conform").setup({
-         formatter_by_ft = {
-            lua = { "stylua" },
-            java = { "prettier" },
-            ts = { "prettier" },
-            xml = { "xmlformatter" },
-            md = { "markdownlint" },
-         },
-      })
-      vim.keymap.set("n", "<leader>mp", function()
-         require("conform").format({
-            lsp_fallback = true,
-            async = false,
-            timeout_ms = 1000,
-         })
-      end)
-   end,
+    "stevearc/conform.nvim",
+    config = function()
+        require("conform").setup({
+            formatter_by_ft = {
+                lua = { "stylua" },
+                java = { "prettier" },
+                ts = { "prettier" },
+                xml = { "xmlformatter" },
+                md = { "markdownlint" },
+                c = { "clang-format" },
+            },
+        })
+        vim.keymap.set("n", "<leader>mp", function()
+            require("conform").format({
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 1000,
+            })
+        end)
+    end,
 }
